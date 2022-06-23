cfe4627de0b72c86c275062b222aec59435379e7
dylanverstraete <dylan_verstraete1@hotmail.com>
2022-06-23 16:03:40 +0200

    fix

--- pallet-tft-bridge/src/lib.rs
+++ pallet-tft-bridge/src/lib.rs
@@ -235,9 +235,11 @@ pub mod pallet {
                 Validators::<T>::put(validator_accounts);
             }
 
-            FeeAccount::<T>::set(self.fee_account.as_ref());
-            WithdrawFee::<T>::set(self.withdraw_fee);
-            DepositFee::<T>::set(self.deposit_fee)
+            if let Some(ref fee_account) = self.fee_account {
+				FeeAccount::<T>::put(fee_account);
+			}
+            WithdrawFee::<T>::put(self.withdraw_fee);
+            DepositFee::<T>::put(self.deposit_fee)
 		}
 	}
 
